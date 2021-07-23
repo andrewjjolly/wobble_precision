40eea5b662c587e19b2073e9e4664f1c9650660a
Andrew Jolly <andrewjjolly@gmail.com>
2021-07-23 12:04:35 +1000

    assign wavelengths function working

--- scripts/wobble_analysis.py
+++ scripts/wobble_analysis.py
@@ -150,13 +150,59 @@ def lsp_snr_at_period(times, rvs, rvs_err):
 
     return snr_at_b, snr_at_c
 
+def assign_wavelengths(data, no_of_orders):
+
+    wavelengths = []
+    wavelengths_err = []
+
+    for order in range(no_of_orders):
+        order_wl = (np.exp(np.mean(data.xs[order][0]))/10)
+        order_wl_err = (np.exp(np.std(data.xs[order][0]))/10)
+        wavelengths.append(order_wl)
+        wavelengths_err.append(order_wl_err)
+
+    wavelengths = np.array(wavelengths)
+    wavelengths_err = np.array(wavelengths_err)
+
+    return wavelengths, wavelengths_err
+
 #%%
-data_dir = '/home/z5345592/projects/gl667c_wobble/results'
+data_dir = '/home/z5345592/projects/gl667c_wobble/results/'
 star_name = 'GL667C'
+
+lit_period_b = 7.2
+lit_period_c = 28.1
+
+lit_amplitude_b = 3.9
+lit_amplitude_c = 1.7
 
 rv_lr_list = [20,50,100,200,400,1000] #list of learning rates that I have results for
 
-for lr in rv_lr_list:
+good_epochs = [ 0,  1,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,
+         13,  14,  15,  16,  17,  18,  19,  20,  21,  22,  23,  24,  25,
+         26,  27,  28,  30,  31,  32,  33,  34,  36,  37,  38,  39,  40,
+         41,  42,  43,  44,  45,  46,  47,  48,  49,  50,  51,  52,  53,
+         54,  55,  56,  57,  58,  59,  60,  61,  62,  63,  64,  65,  66,
+         67,  68,  69,  70,  71,  72,  73,  74,  75,  76,  77,  78,  80,
+         81,  82,  83,  84,  85,  86,  87,  88,  89,  90,  91,  92,  93,
+         94,  95,  96,  97,  98,  99, 100, 101, 102, 103, 104, 106, 107,
+        108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120,
+        121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133,
+        134, 135, 136, 137, 138, 139, 141, 142, 143, 144, 145, 146, 147,
+        148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160,
+        161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173,
+        174, 175, 176]
+
+bad_orders = np.array([False, False, False, False, False, False, False, False, False,
+       False, False, False, False, False, False, False, False, False,
+       False, False, False, False, False, False,  True, False, False,
+       False, False, False, False, False, False, False, False, False,
+       False, False, False, False, False, False, False, False, False,
+       False, False, False, False, False, False, False, False, False,
+       False, False, False,  True, False, False, False, False, False,
+       False,  True,  True, False, False,  True,  True,  True,  True])
+
+ for lr in rv_lr_list:
     
     txt_results_filename = data_dir + 'results_rvs_lr{}.txt'.format(lr)
     hdf5_results_filename = data_dir + 'results_no_bad_orders_lr{}.hdf5'.format(lr)
@@ -164,8 +210,7 @@ for lr in rv_lr_list:
     results, results_hdf5 = load_results(txt_results_filename, hdf5_results_filename)
 
     times = np.array(results['dates'])
-    combined_order_rvs = np.array(results['RV'])
-    combined_order_rvs_err = np.array(results['RV_err'])
+
 
 """
 planned structure of code:
